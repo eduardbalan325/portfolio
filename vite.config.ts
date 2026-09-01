a119c22f763e8d8294e4407372ad3a4869cf48a4
eduardbalan325 <eduardbalan325@gmail.com>
2026-09-01 12:36:09 -0700

    update: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-  },
   base: '/portfolio/',
-  preview: {
-    host: '0.0.0.0',
-    port: 4173,
-    strictPort: true,
-  },
 });
